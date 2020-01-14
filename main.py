807d5343f2859d0cc0a07e8c3de8ec3a461bd942
Emils Jemeljanovs <emils5110@inbox.lv>
2020-01-14 08:28:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def getIndex():
+  return "Hi!"
+
+@app.route('/home')
+def getHome():
+  return render_template('home.html')
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+  return render_template('contact.html', phone = "123")
+
+
+if __name__=="__main__":
+  app.run(threaded= True,port=5050,debug=True)
+  
+
+
+app.run(host = '0.0.0.0', port = 8020)
